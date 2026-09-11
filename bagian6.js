3e6e38ea84603713a770c2de411a12a33186dfbf
chyyaa <nurainiputrichy@gmail.com>
2026-09-11 15:36:51 +0700

    Bagian 6

---
+++ bagian6.js
@@ -0,0 +1,52 @@
+const products = [
+  {
+    id: 1,
+    title: "Laptop",
+    price: 1200,
+    rating: 4.5,
+    stock: 10,
+    category: "laptops",
+    tags: ["computer", "electronics", "office"],
+    dimensions: { width: 30, height: 2, depth: 20 },
+    reviews: [
+      { user: "A", rating: 5, comment: "Good product" },
+      { user: "B", rating: 4, comment: "Worth it" },
+    ],
+  },
+  {
+    id: 2,
+    title: "Smartphone",
+    price: 800,
+    rating: 4.2,
+    stock: 15,
+    category: "phones",
+    tags: ["mobile", "electronics"],
+    dimensions: { width: 7, height: 0.8, depth: 15 },
+    reviews: [
+      { user: "C", rating: 4, comment: "Nice camera" },
+      { user: "D", rating: 5, comment: "Fast" },
+      { user: "E", rating: 3, comment: "Battery so-so" },
+    ],
+  },
+];
+
+function linearSearch(array, target) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === target) return i;
+  }
+  return -1;
+}
+
+const numbers = [10, 20, 30, 40];
+
+console.log("Cari 30:", linearSearch(numbers, 30));
+console.log("Cari 50:", linearSearch(numbers, 50));
+
+function linearSearchProduct(products, targetId) {
+  for (let i = 0; i < products.length; i++) {
+    if (products[i].id === targetId) {
+      return i;
+    }
+  }
+  return -1;
+}
